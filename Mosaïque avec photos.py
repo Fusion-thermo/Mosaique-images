25bef624e25feeec6d758a19b4d4804082ecf602
Jean BERTHAUD <jean.berthaud@outlook.com>
2024-09-04 19:44:26 +0200

    Plus rapide et plus clean

--- Mosaïque avec photos.py
+++ Mosaïque avec photos.py
@@ -1,139 +1,91 @@
 from PIL import Image
+#doc pillow : https://pillow.readthedocs.io/en/stable/reference/Image.html
 import glob
 from datetime import datetime
-#doc pillow : https://pillow.readthedocs.io/en/stable/reference/Image.html
+import os
+from time import time
+import numpy as np
 
-Objectif=Image.open("Objectif.jpg")
-pngdict={}
-NB_LIGNES=15
-NB_COLONNES=15
-LARGEUR_PHOTOS=Objectif.width//NB_COLONNES
-HAUTEUR_PHOTOS=Objectif.height//NB_LIGNES
-
-
-
-#le pixel gardé est le pixel le plus courant dans l'image
-def moyennepixels(image,largeur=LARGEUR_PHOTOS,hauteur=HAUTEUR_PHOTOS):
-	currentmax=(0,0)
-	couleurs={}
-	for x in range(largeur):
-		for y in range(hauteur):
-			rvb=image.getpixel((x,y))
-			if rvb in couleurs.keys():
-				couleurs[rvb]+=1
-				if couleurs[rvb]>currentmax[1]:
-					currentmax=(rvb,couleurs[rvb])
-			else:
-				couleurs[rvb]=0
-	return currentmax[0]
 
 #moyenne pondérée pour chaque composante rgb
-def moyennepixels_ponderee(image,largeur=LARGEUR_PHOTOS,hauteur=HAUTEUR_PHOTOS):
-	red={}
-	green={}
-	blue={}
-	for x in range(largeur):
-		for y in range(hauteur):
-			rvb=image.getpixel((x,y))
-			if rvb[0] in red.keys():
-				red[rvb[0]]+=1
-			else:
-				red[rvb[0]]=1
-			if rvb[1] in green.keys():
-				green[rvb[1]]+=1
-			else:
-				green[rvb[1]]=1
-			if rvb[2] in blue.keys():
-				blue[rvb[2]]+=1
-			else:
-				blue[rvb[2]]=1
-	rouge_num=0
-	for i in red.keys():
-		rouge_num+=i*red[i]
-	rouge=int(rouge_num/sum(red.values()))
-	vert_num=0
-	for i in green.keys():
-		vert_num+=i*green[i]
-	vert=int(vert_num/sum(green.values()))
-	bleu_num=0
-	for i in blue.keys():
-		bleu_num+=i*blue[i]
-	bleu=int(bleu_num/sum(blue.values()))
-	return (rouge,vert,bleu)
+def moyennepixels_ponderee(image):
+	red, green, blue = image.split()
+	# red, green, blue = np.array(red), np.array(green), np.array(blue)
+	red, green, blue = np.mean(red), np.mean(green), np.mean(blue)
+	return red, green, blue
 
 
 def initialiseimages():
 	#on met dans un dictionnaire tous les noms des images dont on déjà trouvé la couleur moyenne, avec cette couleur moyenne associée
-	try:
-		photos_connues=open("Photos avec moyennes connues.txt","r")
-		texte=photos_connues.read()
-		photos_connues.close()
-		separation=texte.split("|")
-		separation.pop()
-		moyenne_pixels_photo={}
-		for i in range(0,len(separation),2):
-			moyenne_pixels_photo[separation[i]]=separation[i+1]
-	except:
-		moyenne_pixels_photo={}
+	database = chemin + "/Photos avec moyennes connues.txt"
+	if os.path.exists(database):
+		with open(database,"r") as photos_connues:
+			texte = photos_connues.read()
+		moyenne_pixels_photo = eval(texte)
+	else:
+		moyenne_pixels_photo = {}
 
 
-	all_images=glob.glob('Images/*')
+	all_images = glob.glob(chemin + '/Images/*')
+	compteur = 0
 	for filename in all_images: #pour toutes les images du dossier source
 		#print(filename)
-		if filename in moyenne_pixels_photo.keys():#si elles ne sont pas déjà dans la liste susmentionnée
+		compteur+=1
+		if filename in moyenne_pixels_photo.values():
 			continue
 		photo = Image.open(filename)
-		photo=photo.resize((700,500))#on les ouvre et on leur done une résolution plus basse (car photos souvent 5000x3000)
-		pngdict[filename] = photo  # référence
-		rgb=moyennepixels_ponderee(photo,photo.width,photo.height)#on trouve leur couleur moyenn et on l'ajoute au dictionnaire
-		print("Photo {0}".format(all_images.index(filename)))
-		moyenne_pixels_photo[filename]=rgb
+		#réduire la taille de l'image avant de regarder le rgb moyen raccourcit un peu le temps de calcul mais peu, autant garder la précision
+		rgb = moyennepixels_ponderee(photo)#on trouve leur couleur moyenne et on l'ajoute au dictionnaire
+		print("Photo {0}".format(compteur))
+		moyenne_pixels_photo[rgb] = filename
 
-	photos_connues=open("Photos avec moyennes connues.txt","w")#le dictionnaire écrit les nouvelles photos avec leurs couleurs moyennes
-	for nom_fichier in moyenne_pixels_photo.keys():
-		photos_connues.write(nom_fichier+"|"+str(moyenne_pixels_photo[nom_fichier])+"|")
-	photos_connues.close()
-
-	print("liste initialisée")
-
-def afficher():
 	#on met dans un dictionnaire tous les noms des images dont on déjà trouvé la couleur moyenne, avec cette couleur moyenne associée
-	moyenne_pixels_photo={}
-	photos_connues=open("Photos avec moyennes connues.txt","r")
-	texte=photos_connues.read()
+	photos_connues = open(chemin + "/Photos avec moyennes connues.txt","w")#le dictionnaire écrit les nouvelles photos avec leurs couleurs moyennes
+	photos_connues.write(str(moyenne_pixels_photo))
 	photos_connues.close()
-	separation=texte.split("|")
-	separation.pop()
-	for i in range(0,len(separation),2):
-		moyenne_pixels_photo[eval(separation[i+1])]=separation[i]
+
+	print("Liste initialisée")
+
+def afficher(Objectif):
+	moyenne_pixels_photo = {}
+	with open(chemin + "/Photos avec moyennes connues.txt","r") as photos_connues:
+		texte = photos_connues.read()
+	moyenne_pixels_photo = eval(texte)
+	save_images_resized = {}
 
 	#le portrait est découpé en rectangles, on trouve la couleur moyenne de chaque zone, on trouve dans le dict
 	#le filename dont la couleur moyenne est la plus proche, et on affiche cette image (redimensionnée) dans le rectangle du portrait.
 	for colonne in range(NB_COLONNES):
 		print("Colonne {0}".format(colonne))
 		for ligne in range(NB_LIGNES):
-			box=(colonne*LARGEUR_PHOTOS,ligne*HAUTEUR_PHOTOS,colonne*LARGEUR_PHOTOS+LARGEUR_PHOTOS,ligne*HAUTEUR_PHOTOS+HAUTEUR_PHOTOS)
-			region=Objectif.crop(box)
-			couleur=moyennepixels_ponderee(region,region.width,region.height)
-			min_difference=10**6
+			box = (colonne*LARGEUR_PHOTOS,ligne*HAUTEUR_PHOTOS,colonne*LARGEUR_PHOTOS+LARGEUR_PHOTOS,ligne*HAUTEUR_PHOTOS+HAUTEUR_PHOTOS)
+			region = Objectif.crop(box)
+			couleur = moyennepixels_ponderee(region)
+			min_difference = 3 * 256
 			for rgb in moyenne_pixels_photo.keys():
-				difference=abs(rgb[0]-couleur[0])+abs(rgb[1]-couleur[1])+abs(rgb[2]-couleur[2])
+				difference = abs(rgb[0]-couleur[0]) + abs(rgb[1]-couleur[1]) + abs(rgb[2]-couleur[2])
 				if difference<min_difference:
-					min_difference=difference
-					filename=moyenne_pixels_photo[rgb]
-			remplacement_region=Image.open(filename)
-			remplacement_region=remplacement_region.resize((LARGEUR_PHOTOS,HAUTEUR_PHOTOS))
+					min_difference = difference
+					filename = moyenne_pixels_photo[rgb]
+			if filename in save_images_resized.keys():
+				remplacement_region = save_images_resized[filename]
+			else:
+				remplacement_region = Image.open(filename)
+				remplacement_region = remplacement_region.resize((LARGEUR_PHOTOS,HAUTEUR_PHOTOS)) #responsable de plus de 95% du temps de calcul
+				save_images_resized[filename] = remplacement_region
 			Objectif.paste(remplacement_region,box)
-	Objectif.save("Rendus/"+str(datetime.now()).replace(":","-")[:-7]+" Rendu taille "+str(NB_COLONNES)+".jpg")
-	#Objectif.show()
+	Objectif = Objectif.crop((0, 0, NB_COLONNES*LARGEUR_PHOTOS, NB_LIGNES*HAUTEUR_PHOTOS))
+	Objectif.save(chemin + "/Rendus/"+str(datetime.now()).replace(":","-")[:-7]+" Rendu "+str(NB_COLONNES)+".jpg")
+
+chemin = "C:/Users/jeanb/OneDrive/Documents/Python/Mosaïque photos/Mosaique-images"
+Objectif = Image.open(chemin + "/Objectif.jpg")
 
 initialiseimages()
-resolution=[1,2,3,6,9,12,15,20,25,37,50,70,90,100,125,150]
-#resolution=[100]
+resolution = [1,2,3,6,9,12,15,20,25,37,50,70,90,100,125,150]
+resolution = [100]
 for i in resolution:
-	print(i)
-	Objectif=Image.open("Objectif.jpg")
-	NB_LIGNES=NB_COLONNES=i
-	LARGEUR_PHOTOS=Objectif.width//NB_COLONNES
-	HAUTEUR_PHOTOS=Objectif.height//NB_LIGNES
-	afficher()
+	print(i,"x",i)
+	NB_LIGNES = NB_COLONNES = i
+	LARGEUR_PHOTOS = Objectif.width//NB_COLONNES
+	HAUTEUR_PHOTOS = Objectif.height//NB_LIGNES
+	afficher(Objectif)
